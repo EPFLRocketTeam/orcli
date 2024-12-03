e8db42077962dc25a8a34661274fe1f58911df51
feltheshovel <fshang100@gmail.com>
2024-12-03 13:05:36 +0100

    comments unzip.py

--- src/unzip.py
+++ src/unzip.py
@@ -1,10 +1,15 @@
+# extract a zip file to the directory of this program
+# takes 1 argument: the path to the zip file to be unzipped
+
 #I copied this code from AI; trust me it works
 import zipfile
 import sys
 
 # Specify the path to your ZIP file
+# sys.argv[1] is the argument passed during execution
 zip_file_path = sys.argv[1]
 # Specify the directory where you want to extract the files
+# ./ refers to the directory containing this program
 extract_to_path = './'
 
 # Create a ZipFile object

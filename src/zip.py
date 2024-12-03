a2a30292f5e402afc6e78c7376f7f783910a15df
feltheshovel <fshang100@gmail.com>
2024-12-03 13:07:17 +0100

    comments zip.py

--- src/zip.py
+++ src/zip.py
@@ -1,6 +1,9 @@
+# zips "rocket.ork" into another .ork file
+# takes 1 argument: the name of the .ork file for "rocket.ork" to be zipped in
+
 #I copied this code from AI; trust me it works
 import zipfile
 import sys
 
-with zipfile.ZipFile(sys.argv[1], 'w') as zipf:
-    zipf.write ('rocket.ork', arcname = 'rocket.ork') #'rocket.ork' is the file to zip
+with zipfile.ZipFile(sys.argv[1], 'w') as zipf: # sys.argv[1] is argument passed during execution
+    zipf.write ('rocket.ork', arcname = 'rocket.ork') # 'rocket.ork' is the file to zip
